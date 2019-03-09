537116616f5f8b50bd9426a34b8c84bc882495a3
Inês <inesamorimalves@gmail.com>
2019-03-09 01:21:05 +0000

    alterei comentários

--- src/Box.cpp
+++ src/Box.cpp
@@ -26,7 +26,7 @@ Box::Box (float comp, float largure, float height, int stack) {
 
 
 	double x, y, z, xx, yy, zz; 
-	double stackC, stackL, stackA; //espaçamento entre as stack de comprimento, largura e altura
+	double stackC, stackL, stackA; //espaçamento entre as stacks de comprimento, largura e altura
 	int i, j;
 
 	stackC = comp / stack; 
@@ -36,14 +36,14 @@ Box::Box (float comp, float largure, float height, int stack) {
 	
 	// FACES DE CIMA E DE BAIXO
 	
-	y = height/ 2; 
-	yy = -y; //y é usado para indicar a altura da face de cima e yy é a altura a que a base de baixo vai estar
-	z = largure / 2; //z assume metade do valor da largura, ficando o poligono centrado no eixo do z
+	y = height/ 2; //cima
+	yy = -y; //baixo 
+	z = largure / 2; //z tem metade do valor da largura -> poligono centrado no eixo do z
 
 	for (i = 0; i < stack; i++){
-		x = -(comp / 2);  // x assume metade do valor do comprimento, ficando o poligono centrado no eixo do x
-		xx = x + stackC;  //incrementa também o xx, para andar sempre à frente do x, para fazer o triângulo
- 		zz = z - stackL;  //decrementa o z, pois temos o z com o seu valor maximo positivo e para o plano ficar centrado temos que ir tirando o espaçamento entre as stack de largur e altura
+		x = -(comp / 2);  // x tem metade do valor do comprimento -> poligono centrado no eixo do x
+		xx = x + stackC;  //xx é ponto auxiliar, incrementa-se para andar sempre à frente do x e fazer o triângulo 
+ 		zz = z - stackL; 
 		for (j = 0; j < stack; j++){
 
 				this->figure->pushvertex(new Vertex(x, y, z));
@@ -62,21 +62,22 @@ Box::Box (float comp, float largure, float height, int stack) {
 				this->figure->pushvertex(new Vertex(x, yy, zz));
 				this->figure->pushvertex(new Vertex(xx, yy, zz));
 
-			x = x + stackC;   //incrementa o x, sendo o x assumindo o valor na proxima camada
-			xx = xx + stackC;//incrementa também o xx, para andar sempre à frente do x, para fazer o triângulo
+			x = x + stackC;   //incrementar x para a proxima camada
+			xx = xx + stackC;//mais uma vez incrementar tb o xx, pela razão que já foi referida
 		}
-		z = z - stackL;// z vai decrementando sempre o valor do espaçamento lateral, para ficar com as stack certas na largureura
+		z = z - stackL;
 	}
 
+	//FACES DA ESQUERDA E DA DIREITA
 	
-	x = -(comp / 2); // face da esqueda
-	xx = -x; // face da direita
-	y = -(height/ 2); //a altura onde se iniciam as faces laterais
+	x = -(comp / 2); // esqueRda
+	xx = -x; // direita
+	y = -(height/ 2); //altura inicial
 
 	for (i = 0; i < stack; i++){
-		z = largure / 2;    //z assume metade do valor da largura, ficando o poligono centrado no eixo do z
-		zz = z - stackL;  //decrementa z, pois temos o z com o seu valor maximo positivo e para o plano ficar centrado temos que ir tirando o espaçamento entre as stack de largureura
-		yy = y + stackA; //para o poligono ficar centrado, começamos com y negativo, sendo o seu valor metade da altura
+		z = largure / 2;    //z tem metade do valor da largura -> poligono centrado no eixo do z
+		zz = z - stackL;
+		yy = y + stackA;
 
 		for (j = 0; j < stack; j++){
 
@@ -100,20 +101,20 @@ Box::Box (float comp, float largure, float height, int stack) {
 				glVertex3f(xx, yy, zz);
 				glVertex3f(xx, yy, z);
 
-			z = z - stackL;   // z vai decrementando sempre o valor do espaçamento lateral, para ficar com as stack certas na largureura
-			zz = zz - stackL;//decrementa o z, pois temos o z com o seu valor maximo positivo e para o plano ficar centrado temos que ir tirando o espaçamento entre as stack de largureura
+			z = z - stackL; 
+			zz = zz - stackL; 		
 		}
-		y = y + stackA; //incrementar na altura o valor do espaçamento, para obter nessa altura as stack indicadas	
+		y = y + stackA; //temos que incrementar na altura o valor do espaçamento para as stacks ficarem direitas	
 	}
 
-	
+	//FACES DA FRENTE E DE TRÁS
 
-	z = largure / 2; //Face da frente
-	zz = -z; // Face trás
-	y = -(height/ 2); //altura que se inicia as stack frente e trás
+	z = largure / 2; //frente
+	zz = -z; //trás
+	y = -(height/ 2); //altura inicial
 
 	for (i = 0; i < stack; i++){
-		x = -(comp / 2);  //valor da coordenada x, onde se começam a desenhar os triângulos
+		x = -(comp / 2);  //valor do x onde se começam a desenhar os triângulos
 		xx = x + stackC;   //valor do x na proxima camada de comprimento
 		yy = y + stackA; //valor do y na proxima camada de altura
 
@@ -135,10 +136,10 @@ Box::Box (float comp, float largure, float height, int stack) {
 				this->figure->pushvertex(new Vertex(x, yy, zz));
 				this->figure->pushvertex(new Vertex(xx, yy, zz));
 
-			x = x + stackC;    //x vai incrementando o valor do espaçamento do comprimento, para ficar com as stack indicadas
-			xx = xx + stackC; //xx também incrementa o valor do espaçamento do comprimento, para ficar sempre na proxima camada no eixo do x	
+			x = x + stackC;  
+			xx = xx + stackC; 	
 		}
-		y = y + stackA; //incrementar na altura o valor do espaçamento, para obter nessa altura as stack indicadas	
+		y = y + stackA;	
 	}
 }
 
